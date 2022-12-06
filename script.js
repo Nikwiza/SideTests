1d05aaafdb1abb4ed9b0c3c0b291bf71b235a12e
Nikwiza <nikola.kerleta1@gmail.com>
2022-12-06 19:50:15 +0100

    Doing nothing, don't worry about it.

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 var elements = document.getElementsByTagName('input');
 var n = elements.length;
-for(var i = 0; i < n; i++){
-    (function(i)
-    {
-    elements[i].onclick = 
-        function()
-        {
-         console.log("This element is  #" + (i+1));
-        };
-    })(i);
+for(let i = 0; i < n; i++)
+{
+    elements[i].onclick = function(){
+        console.log("This is element #" + (i+1));
+    };
 }
+
+// // A moze i
+// var elements = document.getElementsByTagName('input');
+// var n = elements.length;
+// for(var i = 0; i < n; i++){
+//     (function(i)
+//     {
+//     elements[i].onclick = 
+//         function()
+//         {
+//          console.log("This element is  #" + (i+1));
+//         };
+//     })(i);
+// }
 
 // // A moze i:
 
